d3d76c0a801c0e2894a04f7d53d05dca39698234
kris-kolkin <35231857+kris-kolkin@users.noreply.github.com>
2018-05-09 12:55:43 -0500

    Kris commit
    lab 10

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-$(document).ready(function() {
 
-/*
- *
- * Your code here
- *
- *
- *
- */
+var $img = $("img");	
 
-});
+    
+
+    $("#discovery-btn").click(function() {
+        $("button").hide("fast");
+        $("#enterprise").hide("fast");
+		$("#defiant").hide("fast");
+        
+		$("#discovery").animate({"width":"1000px"}, "fast"); });
+	
+	$("#enterprise-btn").click(function() {
+        $("button").hide("fast");
+		$("#defiant").hide("fast");
+        $("#discovery").hide("fast");
+        
+        $("#enterprise").animate({"width":"1000px"}, "fast"); });
+    
+	$("#defiant-btn").click(function() {
+        $("button").hide("fast");
+        $("#enterprise").hide("fast");
+        $("#discovery").hide("fast");
+        
+		$("#defiant").animate({"width":"1000px"}, "fast"); } );
+        
+
+    $(".menu-button").click(function (){
+        
+		var main = $(".main");
+		var menu = $(".menu-bar");
+        
+        if(menu.css("left") == "-160px") {
+            menu.animate({"left":"0px"}, "fast");
+            main.animate({"left":"160px"}, "fast"); }
+        
+		else {
+            menu.animate({"left":"-160px"}, "fast");
+            main.animate({"left":"0px"}, "fast");} 
+			}) ;
